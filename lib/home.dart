6a8f7179175973102fc266f87ddb9e144447d950
DESKTOP-96JSK74\hp <moodbeder150@gmail.com>
2024-11-06 17:14:06 +0200

    add splash_screen and inial home

--- lib/home.dart
+++ lib/home.dart
@@ -5,6 +5,36 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Container(
+       decoration: BoxDecoration(
+        image: DecorationImage(
+          image: Image.asset("assets/images/background.png").image,
+          fit: BoxFit.cover
+        )
+       ),
+      child: Scaffold(
+        backgroundColor:  Colors.transparent,
+        appBar: AppBar(
+          centerTitle:true ,
+          title: Text("Islami" ,style: TextStyle(color: Colors.black, fontSize: 25),),
+        ),
+        body:Container() ,
+        bottomNavigationBar: Theme(
+          data:  Theme.of(context).copyWith(
+            canvasColor: Color(0xffB7935F)
+          ),
+          child: BottomNavigationBar(
+            
+            items: [
+              BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"), 
+              BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+              BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+              
+              
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
